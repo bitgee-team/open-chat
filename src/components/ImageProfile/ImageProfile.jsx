8f2091578d10ba30bb0c1de5e802488ac118b16c
Omar Diaz <omarjdiazv@gmail.com>
2019-12-07 15:45:38 -0300

    Create Imagen Profile

---
+++ src/components/ImageProfile/ImageProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css'
+
+const ImageProfile = ({ url }) => (
+
+<div className="circle" style={{ backgroundImage: `url(${url})`}}/>
+
+);
+
+export default ImageProfile;
+
